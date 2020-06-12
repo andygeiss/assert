cd01f7c74436d3489341c5b1aa97c7254bcbe0ec
andygeiss <4ndygeiss@gmail.com>
2020-06-12 10:20:38 +0200

    name added

--- assert.go
+++ assert.go
@@ -7,10 +7,12 @@ import (
 )
 
 // That ...
-func That(t *testing.T, got, expected interface{}) {
-	a := fmt.Sprintf("%v", got)
-	b := fmt.Sprintf("%v", expected)
-	if !strings.EqualFold(a, b) {
-		t.Errorf("got [%v] but expected [%v]", got, expected)
-	}
+func That(name string, t *testing.T, got, expected interface{}) {
+	t.Run(name, func(t *testing.T) {
+		a := fmt.Sprintf("%v", got)
+		b := fmt.Sprintf("%v", expected)
+		if !strings.EqualFold(a, b) {
+			t.Errorf("got [%v] but expected [%v]", got, expected)
+		}
+	})
 }
